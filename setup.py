30e358bcac944964ea1a71cf419ae20141d142fb
Koen Van Daele <koen_van_daele@telenet.be>
2014-04-24 09:49:03 +0200

    Add py33 classifier.

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='pyramid_rawes',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Framework :: Pyramid',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
         ],
