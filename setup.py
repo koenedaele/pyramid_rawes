a1b3f93f849a05090587f056f0ffdd1cabeac4fa
Koen Van Daele <koen_van_daele@telenet.be>
2014-07-22 20:30:45 +0200

    Switch from nose to pytest.

--- setup.py
+++ setup.py
@@ -11,19 +11,13 @@ requires = [
     'rawes==0.5.3'
     ]
 
-tests_requires = [
-    'nose',
-    'coverage'
-    ]
-
-testing_extras = tests_requires + []
-
-setup(name='pyramid_rawes',
-      version='0.4.1',
-      license='MIT',
-      description='Rawes binding for pyramid',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
+setup(
+    name='pyramid_rawes',
+    version='0.5.0',
+    license='MIT',
+    description='Rawes bindings for pyramid',
+    long_description=README + '\n\n' +  CHANGES,
+    classifiers=[
         'Intended Audience :: Developers',
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
@@ -33,18 +27,14 @@ setup(name='pyramid_rawes',
         'Programming Language :: Python :: 3.3',
         'Framework :: Pyramid',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
-        ],
-      author='Koen Van Daele',
-      author_email='koen_van_daele@telenet.be',
-      url='https://github.com/koenedaele/pyramid_rawes',
-      keywords='pyramid elastic search rawes',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires = requires,
-      tests_require = tests_requires,
-      extras_require = {
-        'testing': testing_extras
-        },
-      test_suite='pyramid_rawes',
-      )
+    ],
+    author='Koen Van Daele',
+    author_email='koen_van_daele@telenet.be',
+    url='https://github.com/koenedaele/pyramid_rawes',
+    keywords='pyramid elastic search rawes',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires = requires,
+    test_suite='pyramid_rawes',
+)
---
+++ tests/fixtures.py
@@ -0,0 +1,33 @@
+# -*- coding: utf8 -*-
+
+import json
+
+def dummy_encoder(obj):
+    return obj
+
+class DummyDecoder(json.JSONDecoder):
+    
+    def __init__(self):
+        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object)
+    
+    def dict_to_object(self, d):
+        for k,v in d.items():
+            d[k] = 'DUMMY'
+        return d
+
+class TestRegistry(object):
+
+    def __init__(self, settings=None):
+
+        if settings is None:
+            self.settings = {}
+        else:
+            self.settings = settings
+
+        self.rawes = None
+
+    def queryUtility(self, iface, name):
+        return self.rawes
+
+    def registerUtility(self, rawes, iface, name):
+        self.rawes = rawes
